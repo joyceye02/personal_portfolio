400d9846282e9da85102ac3ee1f236f31f46c4b9
joyceye02 <97206948+joyceye02@users.noreply.github.com>
2024-07-18 23:42:08 -0700

    redirect change

--- src/components/MultiPageRoutes.js
+++ src/components/MultiPageRoutes.js
@@ -6,24 +6,28 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-route
 
 function RedirectHandler() {
     const location = useLocation();
+    const navigate = useNavigate();
+
     useEffect(() => {
-      const redirect = new URLSearchParams(location.search).get('redirect');
-      if (redirect) {
-        window.history.replaceState(null, '', redirect);
-      }
-    }, [location]);
+        const redirect = new URLSearchParams(location.search).get('redirect');
+        if (redirect) {
+          navigate(redirect);
+        }
+      }, [location, navigate]);
   
     return null;
 }
 export default function MultiPageRoutes() {
     return (
- 
-        <Routes>
-            {/* <RedirectHandler /> */}
-            <Route exact path={'/personal_portfolio'} element={<Home />} />
-            <Route exact path={'/about'} element={<About />} />
-            <Route exact path={'/portfolio'} element={<Portfolio />} />
-        </Routes>
+        <Router>
+            <RedirectHandler />
+            <Routes>
+                <Route exact path={'/personal_portfolio'} element={<Home />} />
+                <Route exact path={'/about'} element={<About />} />
+                <Route exact path={'/portfolio'} element={<Portfolio />} />
+            </Routes>
+        </Router>
+        
        
     )
 }

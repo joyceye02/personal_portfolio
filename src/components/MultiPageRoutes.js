13311ee35e30cc6581ab985e282094edc1325791
joyceye02 <97206948+joyceye02@users.noreply.github.com>
2024-07-18 23:17:27 -0700

    test redirect

--- src/components/MultiPageRoutes.js
+++ src/components/MultiPageRoutes.js
@@ -1,17 +1,29 @@
 import Home from "./home/Home";
 import About from "./about/About";
 import Portfolio from "./portfolio/Portfolio";
-import React from 'react';
+import React, { useEffect }  from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+function RedirectHandler() {
+    const location = useLocation();
+    useEffect(() => {
+      const redirect = new URLSearchParams(location.search).get('redirect');
+      if (redirect) {
+        window.history.replaceState(null, '', redirect);
+      }
+    }, [location]);
+  
+    return null;
+}
 export default function MultiPageRoutes() {
     return (
-        // <Router basename="/personal_portfolio">
+        <Router>
+            <RedirectHandler />
             <Routes>
                 <Route exact path={'/personal_portfolio'} element={<Home />} />
                 <Route exact path={'/about'} element={<About />} />
                 <Route exact path={'/portfolio'} element={<Portfolio />} />
             </Routes>
-        // </Router>
+        </Router>
     )
 }

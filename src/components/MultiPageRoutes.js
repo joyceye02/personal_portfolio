725f05f62b64cf5c7f7c99faf3cccbdb2706185a
joyceye02 <97206948+joyceye02@users.noreply.github.com>
2024-07-19 15:55:41 -0700

    blank solution

--- src/components/MultiPageRoutes.js
+++ src/components/MultiPageRoutes.js
@@ -2,32 +2,16 @@ import Home from "./home/Home";
 import About from "./about/About";
 import Portfolio from "./portfolio/Portfolio";
 import React, { useEffect }  from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 
-function RedirectHandler() {
-    const location = useLocation();
-    const navigate = useNavigate();
 
-    useEffect(() => {
-        const redirect = new URLSearchParams(location.search).get('redirect');
-        if (redirect) {
-          navigate(redirect);
-        }
-      }, [location, navigate]);
-  
-    return null;
-}
 export default function MultiPageRoutes() {
     return (
-        <Router>
-            <RedirectHandler />
             <Routes>
                 <Route exact path={'/personal_portfolio'} element={<Home />} />
                 <Route exact path={'/about'} element={<About />} />
                 <Route exact path={'/portfolio'} element={<Portfolio />} />
             </Routes>
-        </Router>
-        
-       
+          
     )
 }
